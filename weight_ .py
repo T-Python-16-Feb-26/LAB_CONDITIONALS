3356d2bf16e7212684e221457f2da514902378dd
Wajn <wajnaq1@gmail.com>
2026-02-18 13:06:09 +0300

    completed both labs

---
+++ weight_ .py
@@ -0,0 +1,16 @@
+print()
+user_weight = float(input("user wieght: "))
+user_height = float(input("user height: "))
+
+BMI_calculating = user_weight / (user_height * user_height)
+print()
+print(BMI_calculating)
+print()
+if BMI_calculating > BMI_calculating:
+    print("you are overwieght you need to work out more and watch your diet.")
+elif BMI_calculating == BMI_calculating:
+    print("You are fit & healthy.")
+if  BMI_calculating < BMI_calculating:
+     print("You are underweight. Watch your health.")
+
+print()
